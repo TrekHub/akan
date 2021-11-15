e9dde445144e81aabe64e7f9212cc0d2c438b682
TrekHub <teddyomondi001@gmail.com>
2021-11-15 10:43:03 +0300

    fix the modal

--- app.js
+++ app.js
@@ -10,26 +10,16 @@ const lname = document.getElementById('lname')
 
 
 
-
-
-
 btnSubmit.addEventListener('click', (e) => {
 
     e.preventDefault()
     maleArr = [ 'Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    femaleArr = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+    femaleArr = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    arrDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     actualDay = parseInt(day.value)
     actualMonth = parseInt(month.value)
     fullYear = year.value
-    yearArr = fullYear.match(/.{1,2}/g);
-    century = parseInt(yearArr[0]);
-    actualYear = parseInt(yearArr[1]);
-
-    
-
     generateName()
-      
-
 })
 
 
@@ -48,6 +38,6 @@ function generateName() {
     }
 
     modalHeader.innerHTML = `<h5 class="modal-title">${fname.value}  ${lname.value}</h5> `
-    akan.innerHTML = `<p class="fs-4">Your Akan Name is <strong class="text-success">${akanName}</strong></p>`
+    akan.innerHTML = `<p class="fs-4">You were born on <strong class="text-success">${arrDays[weekDay]}</strong>.  Your Akan Name is <strong class="text-success">${akanName}</strong></p>`
 
 }
